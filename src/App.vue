92a95ffc49deed47e3df4293a0705d563c81295b
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-08-05 06:01:10 -0700

    new update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class=" relative ">
     <NavigationList />
   </header>
   <main class="flex font-sans flex-col-reverse my-1 md:pl-32 md:mr-12 md:flex-row place-content-between gap-4">
@@ -13,9 +13,3 @@ import NavigationList from './components/NavigationList.vue';
 import HeroText from './components/HeroText.vue';
 import TheHero from './components/TheHero.vue';
 </script>
-
-<style scoped>
-header {
-  position: relative;
-}
-</style>
